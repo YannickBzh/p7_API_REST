16599220946291f694507394f36c3f41de1cbd57
YannickBzh <guyader.yannick@gmail.com>
2020-05-02 10:22:36 +0200

    Adding form in modal

--- app.js
+++ app.js
@@ -33,67 +33,67 @@ function setMarkers(map, data) {
 
 
 function btnClick() {
-    $('#btnRate').click(function() {
+    $('#btnRate').click(function () {
         $('#modalRate').addClass('d-block').removeClass('d-none');
     })
 }
 
 
-    // APPEL DE LA MAP
-    let map, infoWindow;
-    function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: { lat: 44.8333, lng: -0.5667 },
-            zoom: 14
+// APPEL DE LA MAP
+let map, infoWindow;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 44.8333, lng: -0.5667 },
+        zoom: 14
+    });
+
+    infoWindow = new google.maps.InfoWindow;
+
+    // GEOLOCALISATION
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            let pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Vous êtes ici');
+            infoWindow.open(map);
+            map.setCenter(pos);
+        }, function () {
+            handleLocationError(true, infoWindow, map.getCenter());
         });
-
-        infoWindow = new google.maps.InfoWindow;
-
-        // GEOLOCALISATION
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function (position) {
-                let pos = {
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude
-                };
-
-                infoWindow.setPosition(pos);
-                infoWindow.setContent('Vous êtes ici');
-                infoWindow.open(map);
-                map.setCenter(pos);
-            }, function () {
-                handleLocationError(true, infoWindow, map.getCenter());
-            });
-        } else {
-            // Browser doesn't support Geolocation
-            handleLocationError(false, infoWindow, map.getCenter());
-        }
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
     }
+}
 
-    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-            'Error: The Geolocation service failed.' :
-            'Error: Your browser doesn\'t support geolocation.');
-        infoWindow.open(map);
-    }
-
-
-    function fetchData() {
-        return fetch('public/restos.json', { mode: 'no-cors' })
-            .then(function (res) {
-                return res.json();
-            })
-            .then(restos => {
-                setMarkers(map, restos);
-            })
-            .catch(function (err) {
-                console.log('Problème');
-                console.log(err);
-            })
-    }
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+}
 
 
-    $(document).ready(function () {
-        fetchData()
-    })
+function fetchData() {
+    return fetch('public/restos.json', { mode: 'no-cors' })
+        .then(function (res) {
+            return res.json();
+        })
+        .then(restos => {
+            setMarkers(map, restos);
+        })
+        .catch(function (err) {
+            console.log('Problème');
+            console.log(err);
+        })
+}
+
+
+$(document).ready(function () {
+    fetchData()
+})
