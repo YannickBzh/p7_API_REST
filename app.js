7d92ba73b0fb8b808f92cbe058919a94f5a42fad
YannickBzh <guyader.yannick@gmail.com>
2020-05-02 09:53:18 +0200

    Add button and modal to rate a restaurant - Step 1

--- app.js
+++ app.js
@@ -14,69 +14,86 @@ function setMarkers(map, data) {
             shape: shape,
             title: restaurant.restaurantName
         });
-        marker.addListener('click', function() {
-            let restaurantName = restaurant.restaurantName
+        marker.addListener('click', function () {
+            let restaurantName = restaurant.restaurantName;
             let ratingsA = restaurant.ratings[0].comment;
             let ratingsB = restaurant.ratings[1].comment;
             let paragrapheA = document.createElement("p");
             let paragrapheB = document.createElement("p");
-            document.getElementById('displayRatings').innerHTML = restaurantName;
-            document.getElementById('displayRatings').appendChild(paragrapheA);
+            $('#displayRatings').html(restaurantName);
+            $('#displayRatings').append(paragrapheA);
             paragrapheA.textContent = ratingsA;
-            document.getElementById('displayRatings').appendChild(paragrapheB);
+            $('#displayRatings').append(paragrapheB);
             paragrapheB.textContent = ratingsB;
-          });
-    }
-}
-
-
-// APPEL DE LA MAP
-let map, infoWindow;
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 44.8333, lng: -0.5667 },
-        zoom: 14
-    });
-    
-    infoWindow = new google.maps.InfoWindow;
-
-    // GEOLOCALISATION
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            let pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Vous êtes ici');
-            infoWindow.open(map);
-            map.setCenter(pos);
-        }, function () {
-            handleLocationError(true, infoWindow, map.getCenter());
+            $('<button id="btnRate">Rate this restaurant</button>').appendTo('#displayRatings');
+            btnClick()
         });
-    } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
     }
 }
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-        'Error: The Geolocation service failed.' :
-        'Error: Your browser doesn\'t support geolocation.');
-    infoWindow.open(map);
+
+function btnClick() {
+    $('#btnRate').click(function() {
+        $('#modalRate').addClass('d-block').removeClass('d-none');
+    })
 }
 
 
-fetch('public/restos.json', {mode: 'no-cors'})
-    .then(function (res) {
-        return res.json();
-    })
-    .then(restos => {
-        setMarkers(map, restos);
-    })
-    .catch(function () {
-        console.log('Problème');
+    // APPEL DE LA MAP
+    let map, infoWindow;
+    function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: { lat: 44.8333, lng: -0.5667 },
+            zoom: 14
+        });
+
+        infoWindow = new google.maps.InfoWindow;
+
+        // GEOLOCALISATION
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                let pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+
+                infoWindow.setPosition(pos);
+                infoWindow.setContent('Vous êtes ici');
+                infoWindow.open(map);
+                map.setCenter(pos);
+            }, function () {
+                handleLocationError(true, infoWindow, map.getCenter());
+            });
+        } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, map.getCenter());
+        }
+    }
+
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+            'Error: The Geolocation service failed.' :
+            'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+    }
+
+
+    function fetchData() {
+        return fetch('public/restos.json', { mode: 'no-cors' })
+            .then(function (res) {
+                return res.json();
+            })
+            .then(restos => {
+                setMarkers(map, restos);
+            })
+            .catch(function (err) {
+                console.log('Problème');
+                console.log(err);
+            })
+    }
+
+
+    $(document).ready(function () {
+        fetchData()
     })
